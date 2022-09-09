59d2d2ea49590c9c140f0da681af132da4c1e09c
Nagendra Jimaninta <33000114@Heytorus.com>
2022-09-09 10:17:52 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import {MatDialog} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular8Curd';
+  constructor(private dialog:MatDialog){}
+  openDialog() {
+    const dialogRef = this.dialog.open(DialogComponent);
+
+    /* dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    }); */
+    width:'30%'
+  }
 }
